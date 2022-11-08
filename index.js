6e417d68b16be400efd5496cb12c7cac3daecebf
Tanin Rahman <taninr87@gmail.com>
2022-11-08 20:26:57 +0600

    add mongodb database and sequire data with dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -11,6 +12,22 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send("Smart Tutor server is running!!!");
 })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.flsgo3q.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try {
+    const servicesCollection = client.db('smartTutor').collection('services');
+
+  }
+  finally{
+
+  }
+}
+run().catch(err => console.error(err));
+
 
 
 app.listen(port, () => {
