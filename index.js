ee01914a4794fca34f5910f244cf2c35796491da
Tanin Rahman <taninr87@gmail.com>
2022-11-08 22:42:03 +0600

    load services

--- index.js
+++ index.js
@@ -17,16 +17,25 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.flsgo3q.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
 async function run(){
   try {
-    const servicesCollection = client.db('smartTutor').collection('services');
+    const serviceCollection = client.db('smartTutor').collection('services');
+    
+    app.get('/services',async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    })
 
   }
+
   finally{
 
   }
 }
-run().catch(err => console.error(err));
+run().catch(error => console.error(error));
 
 
 
