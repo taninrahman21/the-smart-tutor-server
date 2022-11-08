aec3ed86122195a0306b99fe07d95d0af4ac2e24
Tanin Rahman <taninr87@gmail.com>
2022-11-08 23:51:02 +0600

    load all the services

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ async function run(){
       const services = await cursor.limit(3).toArray();
       res.send(services);
     })
+    app.get('/allServices',async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
 
   }
 
